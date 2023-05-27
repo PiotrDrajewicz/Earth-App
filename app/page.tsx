5879f218dd9290167e6fd0b805b6452c55f79b17
unknown <drpiterek@gmail.com>
2023-05-27 22:11:19 +0200

    I've done cleanup.

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,11 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { Canvas, useThree  } from "@react-three/fiber";
-// import Earth from './Earth';
 import Box from './Box';
 import { OrbitControls, Stars } from "@react-three/drei";
-// import Model from './Model';
-// import { useLoader } from '@react-three/fiber'
-// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-// import { Earth2 } from './Earth2';
 import GetInfo from "./GetInfo";
 import { WebGL1Renderer, WebGLRenderer } from "three";
 import { Model } from './EarthBallStart3';
-// import { Model } from './Scene';
 import { EarthMarkers } from './EarthMarkers';
 
 
@@ -25,10 +19,8 @@ const HomePage = () => {
             <section className="canvas-section">
                 <Canvas className="canvas" >
                     <OrbitControls enableZoom={false}/>
-                    {/* <Stars /> */}
                     <ambientLight intensity={0.3}/>
                     <directionalLight position={[-2, 3, 2]} intensity={1} />
-                    {/* <GetInfo /> */}
                     <EarthMarkers />
                 </Canvas>
             </section>
