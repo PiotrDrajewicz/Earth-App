ebb6568c13dfab750f0a9532c1056825ea0ceb27
unknown <drpiterek@gmail.com>
2023-05-28 12:02:48 +0200

    Working on the title page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,33 @@
 'use client'
 
-import { useRef } from "react";
+// import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { Canvas, useThree  } from "@react-three/fiber";
 import Box from './Box';
 import { OrbitControls, Stars } from "@react-three/drei";
-import GetInfo from "./GetInfo";
-import { WebGL1Renderer, WebGLRenderer } from "three";
-import { Model } from './EarthBallStart3';
+// import GetInfo from "./GetInfo";
+// import { WebGL1Renderer, WebGLRenderer } from "three";
 import { EarthMarkers } from './EarthMarkers';
 
 
-const HomePage = () => {     
+const HomePage: React.FC = () => {     
 
     return (
         <>
-            <h1>Home for Earth.</h1>
-            <section className="canvas-section">
-                <Canvas className="canvas" >
-                    <OrbitControls enableZoom={false}/>
-                    <ambientLight intensity={0.3}/>
-                    <directionalLight position={[-2, 3, 2]} intensity={1} />
-                    <EarthMarkers />
-                </Canvas>
-            </section>
+            <div className="title-all">
+                <section className="canvas-section">
+                    <Canvas className="canvas" >
+                        <OrbitControls enableZoom={false}/>
+                        <ambientLight intensity={0.3}/>
+                        <directionalLight position={[-2, 3, 2]} intensity={1} />
+                        <EarthMarkers />
+                    </Canvas>
+                </section>
+                <section className="page-title-section">
+                    <h1 className="page-title">GLOBEGUIDER</h1>
+                    <h2 className="page-subtitle">Breathtaking adventures</h2>
+                </section>
+            </div>
         </>
 
     )
